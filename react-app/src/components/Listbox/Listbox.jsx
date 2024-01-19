424e680d915a38200089dae85a2d3990cd9b87d7
VersluysTheo <kanbikil@gmail.com>
2024-01-19 09:24:48 +0100

    Listbox avec données

--- react-app/src/components/Listbox/Listbox.jsx
+++ react-app/src/components/Listbox/Listbox.jsx
@@ -1,37 +1,34 @@
 import styles from './Listbox.module.css'
 
 
-
-
 export default function Listbox(){
-    
-// avec un tableau
 
-// const styleslist = [
-//     { name: 'rock' },
-//     { name: 'jazz' },
-//     { name: 'funk' },
-//     { name: 'rap' },
-//     { name: 'metal' },
-//     { name: 'electronic' },
-//     { name: 'indie' },
-//     { name: 'classical' },
-//     { name: 'opera' },
-//     { name: 'hip-hop' },
-//     { name: 'blues rock' }
-//   ]
-//   styleslist.forEach(element => {
-//     const optionValue = document.createElement('option');
-//     optionValue.value = element.name;
-//     optionValue.textContent = element.name;
-//     const select = document.querySelector('select');
-//     select.appendChild(optionValue);
-//   })
+    // avec un tableau
+
+    let styleslist = [
+        { name: 'rock' },
+        { name: 'jazz' },
+        { name: 'funk' },
+        { name: 'rap' },
+        { name: 'metal' },
+        { name: 'electronic' },
+        { name: 'indie' },
+        { name: 'classical' },
+        { name: 'opera' },
+        { name: 'hip-hop' },
+        { name: 'blues rock' }
+    ];
+    
 
     return(
         <div className={styles.listbox}>
             <select className={styles.selectlist} name="style-select" id="style-select">
                 <option value="">Choississez un style :</option>
+                {styleslist.map(style => {
+                    return(
+                        <option key={style.name} value={style.name}>{style.name}</option>
+                    )
+                })}
             </select>
         </div>
     )
